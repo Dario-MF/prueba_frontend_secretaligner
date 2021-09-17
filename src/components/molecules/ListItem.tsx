e51053f9aff5f5e1947024147edcc438a0fb0182
Dario MF <ferrada_86@hotmail.com>
2021-09-17 12:07:47 +0200

    clientes lista en tabla

---
+++ src/components/molecules/ListItem.tsx
@@ -0,0 +1,74 @@
+import { IPaciente, ObjetivoTratamiento, FichaDental } from '../../interfaces/pacienteInterface';
+
+interface Iprops {
+    paciente: IPaciente;
+}
+
+
+const ListItem = ({ paciente }: Iprops) => {
+    const { datos_paciente, ficha_dental } = paciente;
+
+    const randomColor = (): string => {
+        const hexValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e"];
+        let newColor = "#";
+
+        for (let i = 0; i < 6; i++) {
+            let x = Math.round(Math.random() * 14);
+            let y = hexValues[x];
+            newColor += y;
+        }
+        return newColor;
+    }
+
+    const handleClick = (action: string): void => {
+        // acciones sobre paciente
+    }
+
+    return (
+        <tr className="listItemClient">
+            <td>
+                <div className="listItemClient__dataPersonal">
+                    <div className="listItemClient__dataPersonal__imgIniciales">
+                        <p style={{ backgroundColor: randomColor() }}>
+                            {`${datos_paciente.nombre.charAt(0)}${datos_paciente.apellidos.charAt(0)}`}
+                        </p>
+                    </div>
+                    <div className="listItemClient__dataPersonal__container">
+                        <p className="listItemClient__dataPersonal__nombreCompleto">
+                            {`${datos_paciente.nombre} ${datos_paciente.apellidos}`}
+                        </p>
+                        <p className="listItemClient__dataPersonal__fechaNacimiento">
+                            <i className="bi bi-calendar4-event"></i>
+                            {datos_paciente.fecha_nacimiento}
+                        </p>
+                    </div>
+                </div>
+            </td>
+            <td>
+                <p className="listItemClient__clinica">
+                    {ficha_dental.clinica}
+                </p>
+            </td>
+            <td>
+                <p className="listItemClient__objetivo">
+                    {ficha_dental.objetivo_tratamiento}
+                </p>
+            </td>
+            <td>
+                <p className="listItemClient__estado">
+                    {ficha_dental.estado}
+                </p>
+            </td>
+            <td>
+                <select className="listItemClient__selectAction">
+                    <option >Acciones</option>
+                    <option onClick={() => handleClick('editar')}>Editar</option>
+                    <option onClick={() => handleClick('finalizar')}>Finalizar</option>
+                    <option onClick={() => handleClick('borrar')}>Borrar</option>
+                </select>
+            </td>
+        </tr>
+    )
+}
+
+export default ListItem;
--- src/components/organims/ListadoPacientes.tsx
+++ src/components/organims/ListadoPacientes.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import PacientesContext from "../../context/pacientes/PacientesContext";
 import { IPaciente } from "../../interfaces/pacienteInterface";
+import ListItem from "../molecules/ListItem";
 
 
 const ListadoPacientes = () => {
@@ -8,15 +9,27 @@ const ListadoPacientes = () => {
     const { pacientesState } = useContext(PacientesContext);
 
     return (
-        <ul>
-            {
-                pacientesState.pacientes.map((paciente: IPaciente) => (
-                    <li >{paciente.id}</li>
-
-                ))
-            }
-
-        </ul>
+        <table className="tablaClientes">
+            <thead>
+                <tr>
+                    <th>Nombre y Apellidos</th>
+                    <th>Clinica</th>
+                    <th>Objetivo</th>
+                    <th>Estado</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    pacientesState.pacientes.map((paciente: IPaciente) => (
+                        <ListItem
+                            key={paciente.id}
+                            paciente={paciente}
+                        />
+                    ))
+                }
+            </tbody>
+        </table>
     )
 }
 
